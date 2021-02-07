074c41f06ebd391ae689aaea5f9d78117c0b17da
unknown <elakavicius@gmail.com>
2021-02-07 17:39:35 +0200

    Encoding implementation

--- A7/Encoder.cs
+++ A7/Encoder.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace A7
 {
     public class Encoder
     {
         public string message;
+        public string encodedMessage;
         public int m;
         public int r;
         public int wordsCount;
-        public List<int[]> encodedMessage;
 
         public Encoder(string message, int m, int r)
         {
@@ -22,8 +24,7 @@ namespace A7
 
         public void Encode()
         {
-            BitArray generatingVariables = GetGeneratingVariables(this.wordsCount - 1);
-            int[,] bitviseIntValues = ConvertBitArrayToIntArray(generatingVariables);
+            List<int[]> bitviseIntValues = GetGeneratingVariables(this.wordsCount - 1);
 
             int vectorsCount = this.m + 1;
 
@@ -36,62 +37,153 @@ namespace A7
             }
 
             // Populate product of vectors
-            int[] allVectorsIndexes = new int[this.r];
+            int[] allVectorsIndexes = new int[this.m];
 
-            for (int i = 1; i <= this.m; i ++)
+            for (int i = 0; i < this.m; i ++)
             {
-                allVectorsIndexes[i] = i;
+                allVectorsIndexes[i] = i + 1;
+            }
+
+            foreach(int index in allVectorsIndexes)
+            {
+                Console.Write(index);
             }
 
             List<int[]> allPossibleSubestsOfIndexes = this.CreateSubsets(allVectorsIndexes);
 
-            foreach(var possibleSubset in allPossibleSubestsOfIndexes)
-            {
-                int[] multipliedVector = this.CreateVectorIntArray(bitviseIntValues, 0);
+            Console.WriteLine("All subsets: ");
 
+            foreach (var possibleSubset in allPossibleSubestsOfIndexes)
+            {
+                int[] multipliedVector = genratedVectorsMatrix[0];
+
+                Console.WriteLine("Subset: ");
                 foreach (var index in possibleSubset)
                 {
+                    Console.Write(index);
                     multipliedVector = this.MultiplyVectors(multipliedVector, genratedVectorsMatrix[index]);
                 }
+                Console.WriteLine();
 
                 genratedVectorsMatrix.Add(multipliedVector);
             }
 
-
-        }
-
-        public BitArray GetGeneratingVariables(int maxDecimalvalue)
-        {
-            int[] decimalValues = new int[maxDecimalvalue];
-
-            for (int i = 1; i <= this.wordsCount; i ++)
-            {
-                decimalValues[i - 1] = 1;
-            }
-
-            return new BitArray(decimalValues);
-        }
-
-        public int[,] ConvertBitArrayToIntArray(BitArray bitArray)
-        {
-            int[,] bitviseIntValues = new int[this.wordsCount - 1, this.m - 1];
-
-            for(int i = 0; i < this.wordsCount; i ++)
-            {
-                for (int j = i * m; j <= j + m ; j ++)
+            genratedVectorsMatrix.ForEach(vector => {
+                Console.WriteLine("Matrix vector:");
+                foreach(int value in vector)
                 {
-                    int addedValuesCount = 0;
+                    Console.Write(value);
+                }
+            });
 
-                    bitviseIntValues[i, addedValuesCount] = bitArray[j] ? 1 : 0;
-
-                    addedValuesCount++;
+            int[] messageInBits = this.message.ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            Console.WriteLine("Bits:");
+            foreach (int bit in messageInBits)
+            {
+                Console.Write(bit);
+            }
+            
+            List<int[]> messageIntValues = this.AppendBits(messageInBits, genratedVectorsMatrix.Count);
+            Console.WriteLine("Messages appended: {0}", messageIntValues.Count);
+            foreach (int[] bit in messageIntValues)
+            {
+                Console.WriteLine("Message:");
+                foreach (int value in bit)
+                {
+                    Console.Write(value);
                 }
             }
 
-            return bitviseIntValues;
+            List<int> encodedMessage = this.MultiplyMessageWithGeneratorMatrix(messageIntValues, genratedVectorsMatrix);
+
+            this.encodedMessage = string.Join("", encodedMessage.Select(x => x.ToString()).ToArray());
         }
 
-        public int[] CreateVectorIntArray(int[,] generatingValues, int vectorIndex)
+        public List<int> MultiplyMessageWithGeneratorMatrix(List<int[]> message, List<int[]> generatorMatrix)
+        {
+            List<int> MultipliedMessage = new List<int>();
+
+            for (int i = 0; i < message.Count; i ++)
+            {
+                for (int j = 0; j < this.wordsCount; j ++)
+                {
+                    int result = 0;
+
+                    for (int z = 0; z < message[i].Length; z++)
+                    {
+
+                        result += (message[i][z] * generatorMatrix[z][j]);
+                    }
+
+                    MultipliedMessage.Add(result % 2);
+                }
+            }
+
+            return MultipliedMessage;
+        }
+
+        public List<int[]> GetGeneratingVariables(int maxDecimalvalue)
+        {
+            int[] decimalValues = new int[maxDecimalvalue + 1];
+
+            for (int i = 0; i <= maxDecimalvalue; i ++)
+            {
+                decimalValues[i] = i;
+            }
+
+            List<int[]> allBits = new List<int[]>();
+
+            foreach (int decimalValue in decimalValues)
+            {
+                string s = Convert.ToString(decimalValue, 2);
+
+                int[] bits = s.PadLeft(this.m, '0') // Add 0's from left
+                              .Select(c => int.Parse(c.ToString())) // convert each char to int
+                              .ToArray(); // Convert IEnumerable from select to Array
+
+                allBits.Add(bits);
+            }
+
+            return allBits;
+        }
+
+        public List<int[]> AppendBits(int[] message, int vectorsCount)
+        {
+            int messageSubsetsCount = (int)Math.Ceiling(((double)message.Length) / vectorsCount);
+
+            Console.WriteLine("Message subsets: {0}", messageSubsetsCount);
+            Console.WriteLine("Vectors count: {0}", vectorsCount);
+            Console.WriteLine("Message length: {0}", message.Length);
+
+            int[] appendedBits = new int[messageSubsetsCount * vectorsCount];
+            message.CopyTo(appendedBits, 0);
+
+            if(message.Length < messageSubsetsCount * vectorsCount)
+            {
+                for (int i = message.Length - 1; i < messageSubsetsCount * vectorsCount; i ++)
+                {
+                    appendedBits[i] = 0;
+                }
+            }
+
+            List<int[]> appendedBitsList = new List<int[]>();
+
+            for (int i = 0; i < messageSubsetsCount; i ++)
+            {
+                int[] result = new int[vectorsCount];
+
+                for (int j = 0; j < vectorsCount; j ++)
+                {
+                    result[j] = appendedBits[i * vectorsCount + j];
+                }
+
+                appendedBitsList.Add(result);
+            }
+
+            return appendedBitsList;
+        }
+
+        public int[] CreateVectorIntArray(List<int[]> generatingValues, int vectorIndex)
         {
             int[] vector = new int[this.wordsCount];
 
@@ -100,7 +192,7 @@ namespace A7
                 if (vectorIndex == 0) {
                     vector[i] = 1;
                 } else {
-                    bool isValueAtIndexZero = generatingValues[i, vectorIndex - 1] == 0;
+                    bool isValueAtIndexZero = generatingValues[i][vectorIndex - 1] == 0;
 
                     vector[i] = isValueAtIndexZero ? 1 : 0;
                 }
@@ -142,7 +234,9 @@ namespace A7
             subsets.RemoveAll(subset => subset.Length == 1);
             subsets.RemoveAll(subset => subset.Length > this.r);
 
-            return subsets;
+            List<int[]> orderedSubsets = subsets.OrderBy(subset => subset.Length).ToList();
+
+            return orderedSubsets;
         }
     }
 }
--- UnitTestProject1/EncoderTest.cs
+++ UnitTestProject1/EncoderTest.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using A7;
-
+using System.Collections;
 
 namespace UnitTestProject1
 {
@@ -28,11 +28,11 @@ namespace UnitTestProject1
             expectedResult2.Add(new int[] { 1, 2 });
             expectedResult2.Add(new int[] { 1, 3 });
             expectedResult2.Add(new int[] { 2, 3 });
-            expectedResult2.Add(new int[] { 1, 2, 3 });
             expectedResult2.Add(new int[] { 1, 4 });
             expectedResult2.Add(new int[] { 2, 4 });
-            expectedResult2.Add(new int[] { 1, 2, 4 });
             expectedResult2.Add(new int[] { 3, 4 });
+            expectedResult2.Add(new int[] { 1, 2, 3 });
+            expectedResult2.Add(new int[] { 1, 2, 4 });
             expectedResult2.Add(new int[] { 1, 3, 4 });
             expectedResult2.Add(new int[] { 2, 3, 4 });
             expectedResult2.Add(new int[] { 1, 2, 3, 4 });
@@ -60,6 +60,221 @@ namespace UnitTestProject1
             CollectionAssert.AreEqual(expectedResult2[8], actualResult2[8]);
             CollectionAssert.AreEqual(expectedResult2[9], actualResult2[9]);
             CollectionAssert.AreEqual(expectedResult2[10], actualResult2[10]);
+        }
+
+        [TestMethod]
+        public void TestGetGeneratingVariables()
+        {
+            int[] maxValues = new int[] { 1, 3, 7, 15 };
+
+            List<int[]> expectedResults1 = new List<int[]>();
+            expectedResults1.Add(new int[] { 0 });
+            expectedResults1.Add(new int[] { 1 });
+            Encoder encoder1 = new Encoder("test message", 1, 0);
+            List<int[]> actualResult1 = encoder1.GetGeneratingVariables(maxValues[0]);
+            CollectionAssert.AreEqual(expectedResults1[0], actualResult1[0]);
+            CollectionAssert.AreEqual(expectedResults1[1], actualResult1[1]);
+
+            List<int[]> expectedResults2 = new List<int[]>();
+            expectedResults2.Add(new int[] { 0, 0 });
+            expectedResults2.Add(new int[] { 0, 1 });
+            expectedResults2.Add(new int[] { 1, 0 });
+            expectedResults2.Add(new int[] { 1, 1 });
+            Encoder encoder2 = new Encoder("test message", 2, 0);
+            List<int[]> actualResult2 = encoder2.GetGeneratingVariables(maxValues[1]);
+            CollectionAssert.AreEqual(expectedResults2[0], actualResult2[0]);
+            CollectionAssert.AreEqual(expectedResults2[1], actualResult2[1]);
+            CollectionAssert.AreEqual(expectedResults2[2], actualResult2[2]);
+            CollectionAssert.AreEqual(expectedResults2[3], actualResult2[3]);
+
+            List<int[]> expectedResults3 = new List<int[]>();
+            expectedResults3.Add(new int[] { 0, 0, 0 });
+            expectedResults3.Add(new int[] { 0, 0, 1 });
+            expectedResults3.Add(new int[] { 0, 1, 0 });
+            expectedResults3.Add(new int[] { 0, 1, 1 });
+            expectedResults3.Add(new int[] { 1, 0, 0 });
+            expectedResults3.Add(new int[] { 1, 0, 1 });
+            expectedResults3.Add(new int[] { 1, 1, 0 });
+            expectedResults3.Add(new int[] { 1, 1, 1 });
+            Encoder encoder3 = new Encoder("test message", 3, 0);
+            List<int[]> actualResult3 = encoder3.GetGeneratingVariables(maxValues[2]);
+            CollectionAssert.AreEqual(expectedResults3[0], actualResult3[0]);
+            CollectionAssert.AreEqual(expectedResults3[1], actualResult3[1]);
+            CollectionAssert.AreEqual(expectedResults3[2], actualResult3[2]);
+            CollectionAssert.AreEqual(expectedResults3[3], actualResult3[3]);
+            CollectionAssert.AreEqual(expectedResults3[4], actualResult3[4]);
+            CollectionAssert.AreEqual(expectedResults3[5], actualResult3[5]);
+            CollectionAssert.AreEqual(expectedResults3[6], actualResult3[6]);
+            CollectionAssert.AreEqual(expectedResults3[7], actualResult3[7]);
+        }
+
+        [TestMethod]
+        public void TestCreateVectorIntArray()
+        {
+            List<int[]> generatorValues1 = new List<int[]>();
+            generatorValues1.Add(new int[] { 0 });
+            generatorValues1.Add(new int[] { 1 });
+
+            List<int[]> expectedVectors1 = new List<int[]>();
+            expectedVectors1.Add(new int[] { 1, 1 });
+            expectedVectors1.Add(new int[] { 1, 0 });
+
+            Encoder encoder1 = new Encoder("test message", 1, 0);
+
+            int[] actualVector10 = encoder1.CreateVectorIntArray(generatorValues1, 0);
+            CollectionAssert.AreEqual(expectedVectors1[0], actualVector10);
+
+            int[] actualVector11 = encoder1.CreateVectorIntArray(generatorValues1, 1);
+            CollectionAssert.AreEqual(expectedVectors1[1], actualVector11);
+
+            List<int[]> generatorValues2 = new List<int[]>();
+            generatorValues2.Add(new int[] { 0, 0 });
+            generatorValues2.Add(new int[] { 0, 1 });
+            generatorValues2.Add(new int[] { 1, 0 });
+            generatorValues2.Add(new int[] { 1, 1 });
+
+            List<int[]> expectedVectors2 = new List<int[]>();
+            expectedVectors2.Add(new int[] { 1, 1, 1, 1 });
+            expectedVectors2.Add(new int[] { 1, 1, 0, 0 });
+            expectedVectors2.Add(new int[] { 1, 0, 1, 0 });
+
+            Encoder encoder2 = new Encoder("test message", 2, 0);
+            
+            int[] actualVector20 = encoder2.CreateVectorIntArray(generatorValues2, 0);
+            CollectionAssert.AreEqual(expectedVectors2[0], actualVector20);
+
+            int[] actualVector21 = encoder2.CreateVectorIntArray(generatorValues2, 1);
+            CollectionAssert.AreEqual(expectedVectors2[1], actualVector21);
+
+            int[] actualVector22 = encoder2.CreateVectorIntArray(generatorValues2, 2);
+            CollectionAssert.AreEqual(expectedVectors2[2], actualVector22);
+
+            List<int[]> generatorValues3 = new List<int[]>();
+            generatorValues3.Add(new int[] { 0, 0, 0 });
+            generatorValues3.Add(new int[] { 0, 0, 1 });
+            generatorValues3.Add(new int[] { 0, 1, 0 });
+            generatorValues3.Add(new int[] { 0, 1, 1 });
+            generatorValues3.Add(new int[] { 1, 0, 0 });
+            generatorValues3.Add(new int[] { 1, 0, 1 });
+            generatorValues3.Add(new int[] { 1, 1, 0 });
+            generatorValues3.Add(new int[] { 1, 1, 1 });
+
+
+            List<int[]> expectedVectors3 = new List<int[]>();
+            expectedVectors3.Add(new int[] { 1, 1, 1, 1, 1, 1, 1, 1 });
+            expectedVectors3.Add(new int[] { 1, 1, 1, 1, 0, 0, 0, 0 });
+            expectedVectors3.Add(new int[] { 1, 1, 0, 0, 1, 1, 0, 0 });
+            expectedVectors3.Add(new int[] { 1, 0, 1, 0, 1, 0, 1, 0 });
+
+            Encoder encoder3 = new Encoder("test message", 3, 0);
+
+            int[] actualVector30 = encoder3.CreateVectorIntArray(generatorValues3, 0);
+            CollectionAssert.AreEqual(expectedVectors3[0], actualVector30);
+
+            int[] actualVector31 = encoder3.CreateVectorIntArray(generatorValues3, 1);
+            CollectionAssert.AreEqual(expectedVectors3[1], actualVector31);
+
+            int[] actualVector32 = encoder3.CreateVectorIntArray(generatorValues3, 2);
+            CollectionAssert.AreEqual(expectedVectors3[2], actualVector32);
+
+            int[] actualVector33 = encoder3.CreateVectorIntArray(generatorValues3, 3);
+            CollectionAssert.AreEqual(expectedVectors3[3], actualVector33);
+        }
+
+        [TestMethod]
+        public void TestMultiplyVectors()
+        {
+            Encoder encoder = new Encoder("test message", 2, 0);
+
+            int[] vector1 = new int[] { 1, 0, 1, 0 };
+            int[] vector2 = new int[] { 1, 1, 1, 1 };
+            int[] expectedResult = new int[] { 1, 0, 1, 0 };
+
+            int[] actualResult = encoder.MultiplyVectors(vector1, vector2);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMultiplyMessageWithGeneratorMatrix()
+        {
+            Encoder encoder = new Encoder("test message", 3, 3);
+
+            List<int[]> generatorMatrix = new List<int[]>();
+            generatorMatrix.Add(new int[] { 1, 1, 1, 1, 1, 1, 1, 1 });
+            generatorMatrix.Add(new int[] { 1, 1, 1, 1, 0, 0, 0, 0 });
+            generatorMatrix.Add(new int[] { 1, 1, 0, 0, 1, 1, 0, 0 });
+            generatorMatrix.Add(new int[] { 1, 0, 1, 0, 1, 0, 1, 0 });
+            generatorMatrix.Add(new int[] { 1, 1, 0, 0, 0, 0, 0, 0 });
+            generatorMatrix.Add(new int[] { 1, 0, 1, 0, 0, 0, 0, 0 });
+            generatorMatrix.Add(new int[] { 1, 0, 0, 0, 1, 0, 0, 0 });
+            generatorMatrix.Add(new int[] { 1, 0, 0, 0, 0, 0, 0, 0 });
+
+            List<int[]> message1 = new List<int[]>();
+            message1.Add(new int[] { 1, 1, 1, 1, 0, 0, 0, 1 });
+
+            List<int> expectedResult1 = new List<int>();
+            expectedResult1.Add(1);
+            expectedResult1.Add(1);
+            expectedResult1.Add(1);
+            expectedResult1.Add(0);
+            expectedResult1.Add(1);
+            expectedResult1.Add(0);
+            expectedResult1.Add(0);
+            expectedResult1.Add(1);
+
+            List<int> actualResult1 = encoder.MultiplyMessageWithGeneratorMatrix(message1, generatorMatrix);
+            CollectionAssert.AreEqual(expectedResult1, actualResult1);
+
+            List<int[]> message2 = new List<int[]>();
+            message2.Add(new int[] { 1, 1, 1, 1, 0, 0, 0, 1 });
+            message2.Add(new int[] { 1, 1, 1, 1, 0, 0, 0, 0 });
+            message2.Add(new int[] { 1, 0, 0, 0, 0, 0, 0, 1 });
+
+            List<int> expectedResult2 = new List<int>();
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(0);
+            expectedResult2.Add(1);
+            expectedResult2.Add(0);
+            expectedResult2.Add(0);
+            expectedResult2.Add(1);
+
+            expectedResult2.Add(0);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(0);
+            expectedResult2.Add(1);
+            expectedResult2.Add(0);
+            expectedResult2.Add(0);
+            expectedResult2.Add(1);
+
+            expectedResult2.Add(0);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+            expectedResult2.Add(1);
+
+            List<int> actualResult2 = encoder.MultiplyMessageWithGeneratorMatrix(message2, generatorMatrix);;
+
+            CollectionAssert.AreEqual(expectedResult2, actualResult2);
+        }
+
+        [TestMethod]
+        public void TestEncode()
+        {
+            Encoder encoder1 = new Encoder("11110001", 3, 3);
+            encoder1.Encode();
+            string expectedResult1 = "11101001";
+            Assert.AreEqual(expectedResult1, encoder1.encodedMessage);
+
+            Encoder encoder2 = new Encoder("1111000", 3, 3);
+            encoder2.Encode();
+            string expectedResult2 = "01101001";
+            Assert.AreEqual(expectedResult2, encoder2.encodedMessage);
         }
     }
 }
